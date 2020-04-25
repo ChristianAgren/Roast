17b461e10fe520f8b95cf00a9ed431fc209edb2f
ChristianAgren <55452324+ChristianAgren@users.noreply.github.com>
2020-04-25 22:06:11 +0200

    Worked on room navigation

--- src/components/availableRooms/availableRooms.js
+++ src/components/availableRooms/availableRooms.js
@@ -3,13 +3,132 @@
 import React from "react";
 
 import { UserContext } from "../../contexts/userContext";
+import {
+	Button,
+	Container,
+	Grid,
+	List,
+	ListItem,
+	ListItemAvatar,
+	Avatar,
+	ListItemText,
+	Typography,
+	makeStyles,
+	createStyles,
+} from "@material-ui/core";
+import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+import LockIcon from '@material-ui/icons/Lock';
 
-// import { makeStyles, createStyles } from "@material-ui/core";
-
-// const useStyles = makeStyles((theme) => createStyles({}));
+const useStyles = makeStyles((theme) =>
+	createStyles({
+		mainContainer: {
+			padding: theme.spacing(1.5, 0, 1.5, 0),
+			display: 'flex',
+			flexDirection: 'column',
+			// flexWrap: 'nowrap'
+			alignItems: 'center',
+			// justifyContent: 'center'
+		},
+		hideRoomOverFlow: {
+			width: '100%',
+			overflowX: 'hidden',
+			overflowY: 'auto'
+		},
+		roomsContainer: {
+			display: 'flex',
+			flexDirection: 'column',
+			maxHeight: '35vh',
+			flexWrap: 'nowrap',
+			justifyContent: 'flex-start',
+			'& > .MuiGrid-item': {
+				padding: 0,
+				margin: theme.spacing(1, 0, 1, 3)
+			},
+			'& > .MuiGrid-item:last-child': {
+				padding: theme.spacing(0, 2.2, 0, 0)
+			},
+			'& .MuiButton-contained': {
+				backgroundColor: theme.palette.background.paper,
+				width: '3rem',
+				height: '3.5rem'
+			}
+		},
+		openIcon: {
+			color: 'green',
+			margin: theme.spacing(1, 2.5, 1, 1)
+		}
+	})
+);
 
 function AvailableRooms(props) {
-	// const classes = useStyles();
+	const classes = useStyles();
+
+	const rooms = {
+		open: [
+			{
+				id: 1,
+				name: "Room 1",
+				password: ""
+			},
+			{
+				id: 2,
+				name: "Room 2",
+				password: ""
+			},
+			{
+				id: 4,
+				name: "Room 3",
+				password: ""
+			},
+			{
+				id: 5,
+				name: "this is a very long string",
+				password: ""
+			},
+			{
+				id: 6,
+				name: "short",
+				password: ""
+			},
+			{
+				id: 7,
+				name: "smol",
+				password: ""
+			},
+		],
+		locked: [
+			{
+				id: 3,
+				name: "lol",
+				password: "a"
+			},
+			{
+				id: 3123,
+				name: "lol",
+				password: "a"
+			},
+			{
+				id: 323,
+				name: "lol",
+				password: "a"
+			},
+			{
+				id: 353432,
+				name: "lol",
+				password: "a"
+			},
+			{
+				id: 345645,
+				name: "lol",
+				password: "a"
+			},
+			{
+				id: 375675,
+				name: "lol",
+				password: "a"
+			},
+		]
+	}
 
 	const onJoinClick = (event, joinRoom) => {
 		joinRoom(event);
@@ -21,26 +140,78 @@ function AvailableRooms(props) {
 	return (
 		<UserContext.Consumer>
 			{(user) => (
-				<>
-					<p>rooms</p>
-					<ul>
-						<li
-							onClick={(e) => onJoinClick(e, user.joinRoom)}
-							style={{ cursor: "pointer" }}
-							id="1">
-							hej1
-						</li>
-					</ul>
-					<p>locked rooms</p>
-					<ul>
-						<li
-							onClick={(e) => onJoinClick(e, user.joinRoom)}
-							style={{ cursor: "pointer" }}
-							id="2">
-							då1
-						</li>
-					</ul>
-				</>
+				// <>
+				// 	<p>rooms</p>
+				// 	<ul>
+				// 		<li
+				// 			onClick={(e) => onJoinClick(e, user.joinRoom)}
+				// 			style={{ cursor: "pointer" }}
+				// 			id="1">
+				// 			hej1
+				// 		</li>
+				// 	</ul>
+				// 	<p>locked rooms</p>
+				// 	<ul>
+				// 		<li
+				// 			onClick={(e) => onJoinClick(e, user.joinRoom)}
+				// 			style={{ cursor: "pointer" }}
+				// 			id="2">
+				// 			då1
+				// 		</li>
+				// 	</ul>
+				// </>
+				<Container className={classes.mainContainer}>
+					<Typography variant="overline">Open rooms</Typography>
+					<div className={classes.hideRoomOverFlow}>
+						<Grid container className={classes.roomsContainer}>
+							<List dense>
+								{
+									rooms.open.map((room) =>
+										<ListItem button key={`${room.id}:${room.name}`}>
+											<FiberManualRecordIcon fontSize="small" className={classes.openIcon}/>
+											<ListItemText
+												primary={room.name}
+												secondary="olika användare etc"
+											/>
+										</ListItem>,
+										// <Grid item
+										// 	key={`${room.id}:${room.name}`}
+										// >
+										// 	<Button variant="contained">{room.name}</Button>
+										// </Grid>
+									)
+								}
+							</List>
+						</Grid>
+					</div>
+					<Typography variant="overline">Locked rooms</Typography>
+					<div className={classes.hideRoomOverFlow}>
+						<Grid container className={classes.roomsContainer}>
+							<List dense>
+								{
+									rooms.locked.map((room) =>
+										<ListItem button key={`${room.id}:${room.name}`}>
+											<ListItemAvatar>
+												<Avatar>
+													<LockIcon />
+												</Avatar>
+											</ListItemAvatar>
+											<ListItemText
+												primary={room.name}
+												secondary="olika användare etc"
+											/>
+										</ListItem>,
+										// <Grid item
+										// 	key={`${room.id}:${room.name}`}
+										// >
+										// 	<Button variant="contained">{room.name}</Button>
+										// </Grid>
+									)
+								}
+							</List>
+						</Grid>
+					</div>
+				</Container>
 			)}
 		</UserContext.Consumer>
 	);
