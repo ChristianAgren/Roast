325c91405ce40e3a5084b5349199578922b79758
ChristianAgren <55452324+ChristianAgren@users.noreply.github.com>
2020-04-28 15:07:32 +0200

    commented out unused code

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,10 +1,17 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-module.exports = function(app) {
-  app.use(
-    '/api',
-    createProxyMiddleware({
-      target: 'http://localhost:8080',
-      changeOrigin: true,
-    })
-  );
-};
+// const { createProxyMiddleware } = require('http-proxy-middleware');
+// module.exports = function (app) {
+//   // app.use(
+//   //   '/api',
+//   //   createProxyMiddleware({
+//   //     target: 'http://localhost:8080',
+//   //     changeOrigin: true,
+//   //   })
+//   // );
+//   app.use(proxy('/socket.io',
+//     createProxyMiddleware({
+//       target: 'http://localhost:8080/',
+//       changeOrigin: true,
+//       ws: true,
+//     })
+//   ));
+// };
